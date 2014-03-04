51c482fe8e70ea39497ab4640dbeef5e4569ea57
Christian Johnson <b1bbombersrule@gmail.com>
2014-03-04 17:41:54 -0500

    This scripts converts *NLO.top file to root files with TH1Fs.

---
+++ TopPlotter.py
@@ -0,0 +1,39 @@
+import ROOT
+import os, array
+
+input_file = './Wpm_merged_NLO.top'
+file_name = input_file.split('/')[1]
+file_name = os.path.splitext(file_name)[0]
+root_file = ROOT.TFile(file_name+".root", "RECREATE")
+with open(input_file) as file:
+    startOfHist = False
+    fillHist = False
+    for number,line in enumerate(file):
+        if "#" in line:
+            hist_key = line.split()[1]
+            startOfHist = True
+            line_number = number
+            xbins_low = []
+            bin_cont = []
+            bin_err = []
+        if not line.strip(): 
+            startOfHist = False
+            fillHist = True
+        if startOfHist and (number != line_number):
+            line_content = line.split()
+            xbins_low.append(float(line_content[0]))
+            bin_cont.append(float(line_content[2]))
+            bin_err.append(float(line_content[3]))
+            poss_last_bin = float(line_content[1])
+        if fillHist:
+            xbins_low.append(poss_last_bin)
+            xbins_low_array = array.array('d',xbins_low)
+            histogram = ROOT.TH1F(hist_key,hist_key,len(xbins_low)-1,xbins_low_array)
+            for index in range(len(bin_cont)):
+                histogram.SetBinContent(index+1,bin_cont[index])
+                histogram.SetBinError(index+1,bin_err[index])
+            histogram.Write()
+            del histogram
+            fillHist = False
+            
+root_file.Write()
